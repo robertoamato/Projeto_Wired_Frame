040bd4df2553bbcb66155690b67cab724c03c1ce
robertoamato <robertoamatoz@gmail.com>
2024-09-17 15:49:30 -0300

    segunda fase 1/7

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,17 @@ platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
   .catch(err => console.error(err));
+
+  import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResponsiveService {
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  isSmallScreen(): boolean {
+    return this.breakpointObserver.isMatched(Breakpoints.Handset);
+  }
+}
